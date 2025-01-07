8dfea19ce2542ed9086ca2a2234d67ab872b94d6
Gulshan Yadav <135387702+GulshanYadav9684@users.noreply.github.com>
2025-01-07 10:16:33 +0530

    Pattern added

---
+++ p16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main(int i,int j)
+{
+if(j=i)
+while(i<=5)
+{
+	while(j<=5)
+	{
+	 if(i%2==1)
+		{
+	if(printf("%d ",j++)){}
+		}
+		else
+		{
+				if(printf("* ",j++)){}
+
+		}
+	}
+	if(printf("\n",j=1,i++)){}
+}	
+}
