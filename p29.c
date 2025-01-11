b8dfe2fdb5f2d4f0d604516b67729b3d07dda30c
Gulshan Yadav <135387702+GulshanYadav9684@users.noreply.github.com>
2025-01-11 16:22:31 +0530

    Pattern added

---
+++ p29.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main(int i,int j,int k)
+{
+	if(j=i,k=0){}
+	while(i<=4)
+	{
+		while(j<=i)
+		{
+			if(k==5)
+			{
+				if(k=0){}
+			}
+			if(printf("%d ",k++,j++)){}
+		}
+		//if(printf("\n")){}
+	if(printf("\n",j=1,i++)){}
+	}
+}
