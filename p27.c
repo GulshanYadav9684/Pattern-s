a7480bd174b1c3bf2e1ff717258b5686b577a363
Gulshan Yadav <135387702+GulshanYadav9684@users.noreply.github.com>
2025-01-11 16:12:50 +0530

    Pattern added

---
+++ p27.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int isPrime(int n,int i,int c)
+{
+	//if(int i,int c){}
+	if(i=2,c=1){}
+	{
+		while(i<n)
+		{
+			if(n%i==0)
+				{
+					if(c=0){}
+				}
+		}
+		if(i++){}
+		return **c
+	}
+}
+void main(int i,int a,int b)
+{
+if(a=1,b=1){}
+	{
+		while(i<=45)
+		{
+			if(isPrime(i,0,0))
+			{
+				if(printf("%d ",i)){}
+				if(a==b)
+				{
+					if(printf("\n")){}
+					if(a=0,b++){}
+				}
+				if(a++){}
+			}
+		}
+		if(i++){}
+	}
+}
